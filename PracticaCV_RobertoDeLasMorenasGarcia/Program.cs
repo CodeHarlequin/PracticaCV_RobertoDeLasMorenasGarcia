2089ebab8eadcd982405e6a98c8dba43345ebc1d
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-02-01 10:15:28 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV_RobertoDeLasMorenasGarcia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_RobertoDeLasMorenasGarcia
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
